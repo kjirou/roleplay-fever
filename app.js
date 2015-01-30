8de4f8e0ae0dd1bfecf4f01ea6eb646cb5d5c058
kjirou <kjirou.web@gmail.com>
2015-01-30 20:48:20 +0900

    Arrange error pages

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var express = require('express');
 var session = require('express-session');
+var httpErrors = require('http-errors');
 var _ = require('lodash');
 var logger = require('morgan');
 var passport = require('passport');
@@ -22,6 +23,14 @@ var app = express();
 //
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+//
+// common locals
+//
+app.locals = {
+  basedir: conf.viewRoot
+};
 
 
 //
@@ -56,36 +65,33 @@ app.use(passport.session());
 app.use('/', apps.subApps.core);
 
 
+//
+// handle errors
+//
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  next(httpErrors(404));
 });
 
-//// error handlers
-//
-//// development error handler
-//// will print stacktrace
-//if (app.get('env') === 'development') {
-//    app.use(function(err, req, res, next) {
-//        res.status(err.status || 500);
-//        res.render('error', {
-//            message: err.message,
-//            error: err
-//        });
-//    });
-//}
-//
-//// production error handler
-//// no stacktraces leaked to user
-//app.use(function(err, req, res, next) {
-//    res.status(err.status || 500);
-//    res.render('error', {
-//        message: err.message,
-//        error: {}
-//    });
-//});
-//
+// error handlers
+if (conf.env === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status);
+    res.render('apps/error', {
+      message: err.message,
+      error: err
+    });
+  });
+} else {
+  app.use(function(err, req, res, next) {
+    res.status(err.status);
+    res.render('apps/error', {
+      message: err.message,
+      error: {}
+    });
+  });
+}
+
 
 module.exports = app;
